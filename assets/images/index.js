01ae3abeb8d90ce1dc225a5c2d20221418749bd9
taofeek-ob <taofeek01@yahoo.com>
2022-11-24 10:18:35 +0100

    feat: navbar, hero, map, and why-clime components

---
+++ assets/images/index.js
@@ -0,0 +1,4 @@
+export { default as Logo } from "./logo.webp";
+export { default as Cloud } from "./cloud.gif";
+export { default as SVGMap } from "./mapbase.svg";
+export { default as Line } from "./Line.svg";
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Inter, Space_Grotesk } from "@next/font/google";
+import cx from "classnames";
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ["latin"] });
+const spacegrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={cx(inter.className, spacegrotesk.className)}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;

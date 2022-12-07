54402a2eed84c0043bde5b2ec5c7b4ea8cf0e360
taofeek-ob <taofeek01@yahoo.com>
2022-12-07 22:05:29 +0100

    fix: the changes noted in the documentation

--- components/Timeline/TimelineData.jsx
+++ components/Timeline/TimelineData.jsx
@@ -2,12 +2,12 @@ export const Approach = [
   {
     id: 1,
     title: "Data Agregations",
-    desc: `Climecheck leverages the blockchain for data integrity and verifiability. Users can Download raw sensor data, Train models on the data, Perform confidential computing.`,
+    desc: `Climecheck leverages the blockchain for data integrity and verifiability. Users can:\n  &#x2022; Download raw sensor data\n &#x2022; Train models on the data\n &#x2022; Perform confidential computing`,
   },
   {
     id: 2,
     title: "Decentralized Participation",
-    desc: "Collective ownership of IRL assets geared towards climate science research. Collective ownership of physical infrastructure e.g Radar, Satellite, Labs etc",
+    desc: "Facilitating individual participation through ownership of sensor devices contributing data to the climecheck ecosystem",
   },
   {
     id: 3,

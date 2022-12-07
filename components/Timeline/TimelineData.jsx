f636dea765abb00b8b82dd25bcec202587b8a9a8
taofeek-ob <taofeek01@yahoo.com>
2022-12-07 22:35:00 +0100

    fix: the changes noted in the documentation

--- components/Timeline/TimelineData.jsx
+++ components/Timeline/TimelineData.jsx
@@ -2,7 +2,10 @@ export const Approach = [
   {
     id: 1,
     title: "Data Agregations",
-    desc: `Climecheck leverages the blockchain for data integrity and verifiability. Users can:\n  &#x2022; Download raw sensor data\n &#x2022; Train models on the data\n &#x2022; Perform confidential computing`,
+    desc: `Climecheck leverages the blockchain for data integrity and verifiability. Users can: 
+    • Download raw sensor data
+    • Train models on the data
+    • Perform confidential computing`,
   },
   {
     id: 2,

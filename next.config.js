a2d464221f23e25c1d0f8cf3a1e480e7fd81b64e
taofeek-ob <taofeek01@yahoo.com>
2023-03-03 11:46:30 +0100

    rebase

--- next.config.js
+++ next.config.js
@@ -6,10 +6,14 @@ const nextConfig = {
     defaultLocale: "en",
   },
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
   webpack5: false,
 >>>>>>> 5192e39 (fix)
+=======
+
+>>>>>>> 8e11b66 (fix)
   experimental: { esmExternals: true },
 };
 

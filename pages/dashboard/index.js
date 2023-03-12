0b02fbd64c92e7c6f000205f63d54aa83f541035
taofeek-ob <taofeek01@yahoo.com>
2023-03-12 08:47:12 +0100

    styling improvement

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,5 +1,5 @@
 import Layout from "../../components/Dashboard/Layout";
-import { MdOutlineNotificationsNone } from "react-icons/md";
+
 import { FiDownload } from "react-icons/fi";
 import { AiOutlineCalendar } from "react-icons/ai";
 import Graph from "../../components/Dashboard/Graph";
@@ -20,13 +20,13 @@ const dashboard = () => {
       <Layout title="Dashboard" showSearch={true}>
         <div className="flex flex-col flex-[70] items-center justify-between gap-16 text-black ">
           <div className="md:flex-row flex gap-4 flex-1 text-center justify-between flex-wrap items-stretch sm:items-center w-full">
-            <div className="sm:flex-1 border p-2 rounded-lg">
+            <div className=" border p-2 rounded-lg">
               <span>Polution: </span>
               <select name="select" id="">
                 <option value="Co2">Co2</option>
               </select>
             </div>
-            <div className="hidden sm:flex gap-4 justify-between items-center">
+            <div className="hidden sm:flex md:mx-auto gap-4 justify-between items-center">
               <div className="flex-1 border p-2 gap-x-3 justify-between  rounded-lg flex">
                 <span>From: </span>
                 <input
@@ -37,26 +37,10 @@ const dashboard = () => {
                   max={new Date().toISOString().split("T")[0]}
                 />
               </div>
-             
-                  <div className="flex-1 border p-2 gap-x-3 flex justify-between rounded-lg ">
-                    <span>To: </span>
 
-                    <input
-                      type="date"
-                      name="date"
-                      id="to"
-                      max={new Date().toISOString().split("T")[0]}
-                    />
-                  </div>
-                </div>
-                <div className="flex sm:hidden gap-4 justify-between items-center text-primary">
-                  <div className=" p-4 flex border rounded-lg justify-between items-center">
-                    <AiOutlineCalendar />
-                  </div>
-                  <div className=" p-4 flex border  rounded-lg justify-between items-center">
-                    <FiDownload />
-                  </div>
-                </div>
+              <div className="flex-1 border p-2 gap-x-3 flex justify-between rounded-lg ">
+                <span>To: </span>
+
                 <input
                   type="date"
                   name="date"
@@ -72,32 +56,42 @@ const dashboard = () => {
               <div className=" p-4 flex border  rounded-lg justify-between items-center">
                 <FiDownload />
               </div>
-
             </div>
+            <input
+              type="date"
+              name="date"
+              id="to"
+              max={new Date().toISOString().split("T")[0]}
+            />
+          </div>
 
-            <div className=" hidden flex-1 border p-2 rounded-lg w-max sm:flex items-center justify-between gap-x-1 px-4 bg-[#74BF44] text-white">
-              <div className=" hidden flex-1 border p-2 rounded-lg w-max sm:flex items-center justify-between gap-x-1 px-4 bg-[#74BF44] text-white">
-                <span>Download Now</span>
-                <FiDownload />
-              </div>
+          <div className="flex sm:hidden gap-4 justify-between items-center text-primary">
+            <div className=" p-4 flex border rounded-lg justify-between items-center">
+              <AiOutlineCalendar />
             </div>
-            <div
-              style={{
-                color: "#fff",
-
-                transition: "0.3s ease-in-out",
-                width: "100%",
-                height: "400px",
-                display: "flex",
-                flexDirection: "column",
-                position: "relative",
-              }}
-            >
-              <Graph data={Data} />
+            <div className=" p-4 flex border  rounded-lg justify-between items-center">
+              <FiDownload />
             </div>
           </div>
+
+          <div className=" hidden flex-1 border p-2 rounded-lg w-max sm:flex items-center justify-between gap-x-1 px-4 bg-[#74BF44] text-white">
+            <span>Download Now</span>
+            <FiDownload />
           </div>
-        </div>
+          <div
+            style={{
+              color: "#fff",
+
+              transition: "0.3s ease-in-out",
+              width: "100%",
+              height: "400px",
+              display: "flex",
+              flexDirection: "column",
+              position: "relative",
+            }}
+          >
+            <Graph data={Data} />
+          </div>
         </div>
       </Layout>
     </>
